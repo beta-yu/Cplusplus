f1978c1d8b60aba51cfa0d73b00fd80a2e586cca
qiyu56 <qiyu56@qq.com>
2019-04-07 13:50:51 +0800

    SharedPtr线程安全、循环引用、RAII思想

--- SmartPointer/SmartPointer/Test.cpp
+++ SmartPointer/SmartPointer/Test.cpp
@@ -239,6 +239,7 @@ public:
 //	QIYU::UniquePtr<Date> upd2 = upd;
 //}
 
+#include <mutex>
 /* 模拟实现shared_ptr */
 namespace QIYU
 {
@@ -249,6 +250,7 @@ namespace QIYU
 		SharedPtr(T* ptr)
 			:_ptr(ptr)
 			, _count(new int)
+			, _pmutex(new mutex)
 		{
 			*_count = 1; //新资源刚被放进对象，引用计数为1
 		}
@@ -257,36 +259,27 @@ namespace QIYU
 		{
 			if (_ptr != nullptr)
 			{
-				--(*_count);
-				if (*_count == 0)
-				{
-					delete _ptr;
-					delete _count;
-					cout << "deleted!" << endl;
-				}
+				Release();
 			}
 		}
 
 		SharedPtr(SharedPtr<T>& sp)
 			:_ptr(sp._ptr)
 			, _count(sp._count)
+			, _pmutex(sp._pmutex)
 		{
-			++(*_count);
+			AddCount();
 		}
 
 		SharedPtr<T>& operator=(SharedPtr<T>& sp)
 		{
 			if (_ptr != sp._ptr) //如果原本两对象就指向同一资源，就不用做什么了
 			{
-				--(*_count);
-				//如果引用计数减为0，释放资源
-				if (*_count == 0)
-				{
-					delete _ptr;
-				}
+				Release();
 				_ptr = sp._ptr;
 				_count = sp._count;
-				++(*_count);
+				_pmutex = sp._pmutex;
+				AddCount();
 			}
 		}
 
@@ -300,26 +293,192 @@ namespace QIYU
 			return *_ptr;
 		}
 
+		int use_count()
+		{
+			return *_count;
+		}
+
 
 	private:
 		T* _ptr;
 		int* _count;
+		mutex* _pmutex;  //互斥锁，所对象共享同一把锁
+
+		void AddCount()
+		{
+			_pmutex->lock();
+			++(*_count);
+			_pmutex->unlock();
+		}
+
+		void Release()
+		{
+			bool deleteflag = false;
+			_pmutex->lock();
+			--(*_count);
+			if (*_count == 0)
+			{
+				delete _ptr;
+				deleteflag = true;
+				cout << "deleted!" << endl;
+			}
+			_pmutex->unlock();
+			/*如果只对（--(*_count);）加锁，可能会导致资源被释放两次
+			（当线程1减操作刚完成被切出，假如此时刚好被减为1，线程2在进行减操作，到0，释放资源，
+			等到线程1再次执行，在if (*_count == 0)时读到引用计数为0，再次释放）*/
+			if (deleteflag == true)
+				delete _pmutex;  //释放锁资源
+		}
 	};
 }
 
-void Test()
+//void Test()
+//{
+//	QIYU::SharedPtr<Date> spd(new Date(2019, 4, 1));
+//	QIYU::SharedPtr<Date> spd1(spd);
+//	cout << spd1->_year << endl;
+//	cout << spd1->_month << endl;
+//	cout << spd1->_day << endl;
+//		
+//	//
+//	cout << spd->_year << endl;
+//	cout << spd->_month << endl;
+//	cout << spd->_day << endl;
+//}
+
+#include <thread>
+
+//void SharedPtrFunc(QIYU::SharedPtr<int>& p, int n)
+//{
+//	while (n--)
+//	{
+//		QIYU::SharedPtr<int> copy(p);
+//		
+//	}
+//}
+//
+//void Test()
+//{
+//	QIYU::SharedPtr<int> spi(new int(0));
+//	int n = 10000;
+//	thread t1(SharedPtrFunc, spi, n);
+//	thread t2(SharedPtrFunc, spi, n);
+//
+//	t1.join();
+//	t2.join();
+//}
+
+//void SharedPtrFunc(QIYU::SharedPtr<Date>& p, int n)
+//{
+//	while (n--)
+//	{
+//		++(p->_year);
+//		++(p->_month);
+//		++(p->_day);
+//	}
+//}
+//
+//void Test()
+//{
+//	QIYU::SharedPtr<Date> spd(new Date(1, 1, 1));
+//	int n = 10000;
+//	thread t1(SharedPtrFunc, spd, n);
+//	thread t2(SharedPtrFunc, spd, n);
+//
+//	t1.join();
+//	t2.join();
+//
+//	cout << spd->_year << " " << spd->_month << " " << spd->_day << endl;
+//}
+
+//void SharedPtrFunc(shared_ptr<Date>& p, int n)
+//{
+//	while (n--)
+//	{
+//		++(p->_year);
+//		++(p->_month);
+//		++(p->_day);
+//	}
+//}
+//
+//void Test()
+//{
+//	shared_ptr<Date> spd(new Date(1, 1, 1));
+//	int n = 10000;
+//	thread t1(SharedPtrFunc, spd, n);
+//	thread t2(SharedPtrFunc, spd, n);
+//
+//	t1.join();
+//	t2.join();
+//
+//	cout << spd->_year << " " << spd->_month << " " << spd->_day << endl;
+//}
+
+//struct ListNode
+//{
+//	int _val;
+//	weak_ptr<ListNode> _next;
+//	weak_ptr<ListNode> _prev;
+//
+//	~ListNode()
+//	{
+//		cout << "~ListNode" << endl;
+//	}
+//};
+//
+//void Test()
+//{
+//	shared_ptr<ListNode> Node1(new ListNode);
+//	shared_ptr<ListNode> Node2(new ListNode);
+//
+//	cout << Node1.use_count() << endl;
+//	cout << Node2.use_count() << endl;
+//
+//	Node1->_next = Node2;
+//	Node2->_prev = Node1;
+//
+//	cout << Node1.use_count() << endl;
+//	cout << Node2.use_count() << endl;
+//}
+
+//template<class T>
+//class FreeDel
+//{
+//public:
+//	void operator()(T* ptr)
+//	{
+//		free(ptr);
+//		cout << "free" << endl;
+//	}
+//};
+//
+//void Test()
+//{
+//	FreeDel<int> freefunc; //仿函数删除器
+//	//malloc产生的资源，应通过free释放
+//	shared_ptr<int> spi((int*)malloc(sizeof(int)* 100), freefunc);
+//}
+
+template <class Mutex>
+class LockGuard
 {
-	QIYU::SharedPtr<Date> spd(new Date(2019, 4, 1));
-	QIYU::SharedPtr<Date> spd1(spd);
-	cout << spd1->_year << endl;
-	cout << spd1->_month << endl;
-	cout << spd1->_day << endl;
-		
-	//
-	cout << spd->_year << endl;
-	cout << spd->_month << endl;
-	cout << spd->_day << endl;
-}
+public:
+	LockGuard(Mutex& mtx)
+		:_mutex(mtx)
+	{
+		_mutex.lock();
+	}
+
+	~LockGuard()
+	{
+		_mutex.unlock();
+	}
+
+	LockGuard(const LockGuard<Mutex>& lg) = delete;
+
+private:
+	Mutex& _mutex; //必须是引用，否则会发生拷贝构造，管理的锁不是实际new得到的锁。
+};
 
 int main()
 {
